7850a1e7ac84a057534cc2111ea97b5411be746f
baileywickham <baileywickham@gmail.com>
2022-01-08 15:03:43 -0800

    update

--- mile1/type-checker.rkt
+++ mile1/type-checker.rkt
@@ -37,31 +37,43 @@
          [global-tenv (build-tenv (Mini-decs mini) types)])
     (for ([fun (Mini-funs mini)])
       (let ([tenv (hash-union (build-tenv (append (Fun-params fun) (Fun-decs fun)) types) global-tenv
-                              #:combine (λ (loc glob) loc))])
+                              #:combine (λ (loc glob) loc))]
+            [ret-type (Fun-ret-type fun)])
         (for ([stmt (Fun-body fun)])
-          (type-check-stmt stmt structs fun-sigs tenv))))))
+          (type-check-stmt stmt structs fun-sigs tenv ret-type))))))
 
 ;;
-(define (type-check-stmt stmt structs fun-sigs tenv)
+(define (type-check-stmt stmt structs fun-sigs tenv ret-type)
   (match stmt
-    [(Block stmts) (for ([stmt stmts]) (type-check-stmt stmt structs fun-sigs tenv))]
+    [(Block stmts) (for ([stmt stmts]) (type-check-stmt stmt structs fun-sigs tenv ret-type))]
     [(Assign target src) (let ([target (type-check-exp target structs fun-sigs tenv)]
                                [src (type-check-exp src structs fun-sigs tenv)])
-                           (unless (equal? src target)
+                           (unless (type-equal? target src structs)
                              (type-error "mismatched assignment types: ~e ~e" target src)))]
     [(If guard then else) (let ([guard (type-check-exp guard structs fun-sigs tenv)])
                             (unless (equal? 'bool guard)
                               (type-error "if guard expected bool, got ~e" guard))
-                            (type-check-stmt then structs fun-sigs tenv)
-                            (type-check-stmt else structs fun-sigs tenv))]
+                            (type-check-stmt then structs fun-sigs tenv ret-type)
+                            (unless (null? else) (type-check-stmt else structs fun-sigs tenv ret-type)))]
     [(While guard body) (let ([guard (type-check-exp guard structs fun-sigs tenv)])
                           (unless (equal? 'bool guard)
                             (type-error "while guard expected bool, got ~e" guard))
-                          (type-check-stmt body structs fun-sigs tenv))]
+                          (type-check-stmt body structs fun-sigs tenv ret-type))]
     [(Print exp _) (let ([exp (type-check-exp exp structs fun-sigs tenv)])
                      (unless (equal? 'int exp)
                        (type-error "print expected int, got ~e" exp)))]
-    [o o]))
+    [(Return exp) (let ([exp-type (type-check-exp exp structs fun-sigs tenv)])
+                        (unless (equal? exp-type ret-type)
+                          (type-error "expected return type ~e got ~e" ret-type exp-type)))]
+    [(Return-void) (unless (equal? ret-type 'void) (type-error "expected return type ~e got void" ret-type))]
+    [(Inv id args) (let* ([fun-type (hash-ref fun-sigs id (lambda () (type-error "undefined function ~e" id)))]
+                          [arg-types (map (lambda (a) (type-check-exp a structs fun-sigs tenv)) args)]
+                          [param-types (Fun-type-params fun-type)])
+                     (if (and (equal? (length arg-types) (length param-types))
+                              (andmap (lambda (actual expected) (type-equal? expected actual structs))
+                                  arg-types param-types))
+                         (Fun-type-ret fun-type)
+                         (type-error "cound not call ~e with arguments ~e" id arg-types)))]))
 
 ;;
 (define (type-check-exp exp structs fun-sigs tenv)
@@ -76,8 +88,27 @@
                               (if (and (equal? left right) (equal? (car type-sig) left))
                                   (cdr type-sig)
                                   (type-error "~e: invaid types ~e ~e" op left right)))]
-    
-    ))
+    [(? integer? i) 'int]
+    [(? boolean? b) 'bool]
+    [(? symbol? s) (hash-ref tenv s (lambda () (type-error "unbound identifier: ~e" s)))]
+    [(New id) (if (hash-has-key? structs id)
+                  id
+                  (type-error "undefined struct type: ~e" id))]
+    [(Read) 'int]
+    [(Null) 'null]
+    [(Unary op exp) (let ([type-sig (hash-ref unary-types op)]
+                          [exp-type (type-check-exp exp structs fun-sigs tenv)])
+                      (if (equal? type-sig exp-type)
+                          exp-type
+                          (type-error "could not invoke ~e on ~e" op exp-type)))]
+    [(Inv id args) (let* ([fun-type (hash-ref fun-sigs id (lambda () (type-error "undefined function ~e" id)))]
+                          [arg-types (map (lambda (a) (type-check-exp a structs fun-sigs tenv)) args)]
+                          [param-types (Fun-type-params fun-type)])
+                     (if (and (equal? (length arg-types) (length param-types))
+                              (andmap (lambda (actual expected) (type-equal? expected actual structs))
+                                  arg-types param-types))
+                         (Fun-type-ret fun-type)
+                         (type-error "cound not call ~e with arguments ~e" id arg-types)))]))
 
 ;;
 (define (extract-Fun-type fun types)
@@ -88,6 +119,9 @@
     (unless (or (equal? 'void ret-type) (set-member? types ret-type))
       (type-error "unrecognized return type: ~e" ret-type))
     (Fun-type param-types ret-type)))
+
+(define (type-equal? expected actual structs)
+  (or (and (equal? actual 'null) (hash-has-key? structs expected)) (equal? expected actual)))
 
 ;(define (check-inv inv types tenv))
 
