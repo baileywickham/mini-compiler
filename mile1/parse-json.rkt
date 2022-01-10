21f91f8d0205348f9954a8d0f4b76989eae30801
Alex MacLean <alex.maclean2000@gmail.com>
2022-01-10 11:16:44 -0800

    cleanup

--- mile1/parse-json.rkt
+++ mile1/parse-json.rkt
@@ -7,21 +7,22 @@
 
 (define (parse jsn)
   (Mini (map parse-type (hash-ref jsn 'types))
-        (map parse-dec (hash-ref jsn 'declarations))
-        (map parse-fun (hash-ref jsn 'functions))))
+        (map parse-dec  (hash-ref jsn 'declarations))
+        (map parse-fun  (hash-ref jsn 'functions))))
 
 (define (parse-type type)
   (Struct (string->symbol (hash-ref type 'id)) (map parse-dec (hash-ref type 'fields))))
 
 (define (parse-dec dec)
-  (cons (string->symbol (hash-ref dec 'id)) (string->symbol (hash-ref dec 'type))))
+  (cons (string->symbol (hash-ref dec 'id))
+        (string->symbol (hash-ref dec 'type))))
 
 (define (parse-fun fun)
   (Fun (string->symbol (hash-ref fun 'id))
-        (map parse-dec (hash-ref fun 'parameters))
-        (string->symbol (hash-ref fun 'return_type))
-        (map parse-dec (hash-ref fun 'declarations))
-        (map parse-stmt (hash-ref fun 'body))))
+       (map parse-dec  (hash-ref fun 'parameters))
+       (string->symbol (hash-ref fun 'return_type))
+       (map parse-dec  (hash-ref fun 'declarations))
+       (map parse-stmt (hash-ref fun 'body))))
 
 (define (parse-stmt stmt)
   (match stmt
@@ -32,14 +33,14 @@
      (If (parse-exp guard) (parse-stmt then) (parse-stmt else))]
     [(hash-table ('stmt "if") ('guard guard) ('then then))
      (If (parse-exp guard) (parse-stmt then) '())]
-    [(hash-table ('stmt "print") ('exp exp) ('endl endl)) (Print (parse-exp exp) endl)]
     [(hash-table ('stmt "while") ('guard guard) ('body body))
      (While (parse-exp guard) (parse-stmt body))]
     [(hash-table ('stmt "invocation") ('id id) ('args args))
      (Inv (string->symbol id) (map parse-exp args))]
+    [(hash-table ('stmt "print") ('exp exp) ('endl endl)) (Print (parse-exp exp) endl)]
+    [(hash-table ('stmt "delete") ('exp exp)) (Delete (parse-exp exp))]
     [(hash-table ('stmt "return") ('exp exp)) (Return (parse-exp exp))]
-    [(hash-table ('stmt "return")) (Return (void))]
-    [(hash-table ('stmt "delete") ('exp exp)) (Delete (parse-exp exp))]))
+    [(hash-table ('stmt "return")) (Return (void))]))
 
 (define (parse-exp exp)
   (match exp
@@ -52,10 +53,10 @@
      (Inv (string->symbol id) (map parse-exp args))]
     [(hash-table ('exp "binary") ('operator op) ('lft lft) ('rht rht))
      (Binary (string->symbol op) (parse-exp lft) (parse-exp rht))]
-    [(hash-table ('exp "dot") ('left left) ('id id)) (Dot (parse-exp left) (string->symbol id))]
-    [(hash-table ('exp "new") ('id id)) (New (string->symbol id))]
     [(hash-table ('exp "unary") ('operator op) ('operand exp))
      (Unary (string->symbol op) (parse-exp exp))]
+    [(hash-table ('exp "dot") ('left left) ('id id)) (Dot (parse-exp left) (string->symbol id))]
+    [(hash-table ('exp "new") ('id id)) (New (string->symbol id))]
     [(hash-table ('exp "read")) (Read)]))
 
 (define (parse-target target)
