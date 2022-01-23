34804b25e80cc8583b8022a810bfde8773440186
Alex Maclean (WSL) <alex.maclean2000@gmail.com>
2022-01-23 13:26:04 -0800

    more cleanup

--- mini-compiler/translate-llvm.rkt
+++ mini-compiler/translate-llvm.rkt
@@ -64,124 +64,129 @@
 (define (translate-stmt s locs structs funs)
   (define stmts (box '()))
   (define add-stmt! (λ (s) (set-box! stmts (cons s (unbox stmts)))))
-  (define last-stmts (translate-stmt* s (list locs structs funs add-stmt!)))
+  (define last-stmts ((translate-stmt** locs structs funs add-stmt!) s))
   (append (reverse (unbox stmts)) last-stmts))
 
 ;;
-(define (translate-stmt* s context)
-  (match s
-    [(Goto* label) (list (BrLL (% label)))]
-    [(GotoCond* cond iftrue iffalse)
-     (match-let ([(cons new-cond _) (ensure-type (translate-arg cond context) bit context)])
-       (list (BrCondLL new-cond (% iftrue) (% iffalse))))]
-    [(Return (? void?)) (list (ReturnLL 'void (void)))]
-    [(Return id)
-     (match-let ([(cons ret-id ret-ty) (translate-arg id context)])
-       (list (ReturnLL ret-ty ret-id)))]
-    [(Assign target (Read))
-     (match-let ([(cons loc-id loc-ty) (translate-assign-target target context)])
-       (list
-        (CallLL i32 (@ 'scanf)
-                (list (cons "getelementptr inbounds ([5 x i8], [5 x i8]* @.read, i32 0, i32 0)"
-                            (PtrLL byte))
-                      (cons loc-id (PtrLL loc-ty))) #t)))]
-    [(Assign target src)
-     (match-let ([(cons src-id _) (ensure-type (translate-arg src context) int context)]
-                 [(cons target-id target-ty) (translate-assign-target target context)])
-       (list (StoreLL target-ty src-id target-id)))]
-    [(? Inv?)
-     (match-let ([(cons call _) (translate-inv s context)])
-       (list call))]
-    [(Delete exp)
-     (match-let ([(cons arg-id arg-ty) (translate-arg exp context)])
-       (with-tmp (tmp)
-         (list (AssignLL tmp (CastLL 'bitcast arg-ty arg-id (PtrLL byte)))
-               (CallLL 'void (@ 'free) (list (cons tmp (PtrLL byte))) #f))))]
-    [(Print exp endl?)
-     (let ([arg (translate-arg exp context)])
-       (list
-        (CallLL i32 (@ 'printf)
-                (list (cons (format "getelementptr inbounds ([6 x i8], [6 x i8]* @.~a, i32 0, i32 0)"
-                                    (if endl? 'println 'print))
-                            (PtrLL byte)) arg) #t)))]))
+(define (translate-stmt** locs structs funs add-stmt!)
 
-;;
-(define+ (translate-assign-target target (and context (list locs _ _ _)))
-  (if (Dot? target) (translate-dot target context) (hash-ref locs target)))
-
-;;
-(define+ (translate-arg arg (and context (list locs structs _ add-stmt!)))
-  (match arg
-    [(? boolean?) (cons arg bit)]
-    [(? integer?) (cons arg int)]
-    [(Null) (cons 'null int)]
-    [(? symbol?)
-     (match-let ([(cons id ty) (hash-ref locs arg)])
-       (with-tmp (tmp)
-         (add-stmt! (AssignLL tmp (LoadLL ty id)))
-         (cons tmp ty)))]
-    [(Prim op (list op1 op2))
-     (match-let*
-         ([(list op-name in-ty out-ty) (hash-ref ops op)]
-          [(cons arg1-id op-ty) (ensure-type (translate-arg op1 context) in-ty context)]
-          [(cons arg2-id _)     (ensure-type (translate-arg op2 context) in-ty context)])
-       (with-tmp (tmp)
-         (add-stmt! (AssignLL tmp (BinaryLL op-name op-ty arg1-id arg2-id)))
-         (cons tmp out-ty)))]
-    [(Prim '- (list exp))
-     (match-let ([(cons arg-id _) (translate-arg exp context)])
-       (with-tmp (tmp)
-         (add-stmt! (AssignLL tmp (BinaryLL 'sub int 0 arg-id)))
-         (cons tmp int)))]
-    [(Prim '! (list exp))
-     (match-let ([(cons arg-id _) (ensure-type (translate-arg exp context) bit context)])
-       (with-tmp (tmp)
-         (add-stmt! (AssignLL tmp (BinaryLL 'xor bit #t arg-id)))
-         (cons tmp bit)))]
-    [(Dot left id)
-     (match-let ([(cons dot-id dot-ty) (translate-dot arg context)])
-       (with-tmp (tmp)
-         (add-stmt! (AssignLL tmp (LoadLL dot-ty dot-id)))
-         (cons tmp dot-ty)))]
-    [(Inv id args)
-     (match-let ([(cons call ret-ty) (translate-inv arg context)])
-       (with-tmp (tmp)
-         (add-stmt! (AssignLL tmp call))
-         (cons tmp ret-ty)))]
-    [(New id)
-     (let* ([ty (translate-type id)]
-            [size (* (/ int-size byte-size) (length (hash-ref structs ty)))])
-       (with-tmp (tmp tmp2)
-         (add-stmt! (AssignLL tmp (CallLL (PtrLL byte) (@ 'malloc) (list (cons size i32)) #f)))
-         (add-stmt! (AssignLL tmp2 (CastLL 'bitcast (PtrLL byte) tmp ty)))
-         (cons tmp2 ty)))]))
-
-;;
-(define+ (translate-dot (Dot left id) (and context (list _ structs _ add-stmt!)))
-  (match-let* ([(cons left-id left-ty) (translate-arg left context)]
-               [fields (hash-ref structs left-ty)])
-    (with-tmp (tmp)
-      (add-stmt! (AssignLL tmp (GetEltLL (PtrLL-to left-ty) left-id (index-of (map car fields) id))))
-      (cons tmp (cdr (findf (λ+ ((cons mem-id _)) (equal? mem-id id)) fields))))))
-
-;;
-(define+ (translate-inv (Inv id args) (and context (list _ _ funs _)))
-  (match-let*
-      ([(cons ret-ty param-tys) (hash-ref funs id)]
-       [new-args (map (λ (arg param-ty) (ensure-type (translate-arg arg context) param-ty context))
-                      args param-tys)])
-    (cons (CallLL ret-ty (@ id) new-args #f) ret-ty)))
-
-;;
-(define+ (ensure-type (and dec (cons id ty)) new-ty (list _ _ _ add-stmt!))
-  (match* (ty new-ty)
-    [((IntLL s) (IntLL s2))
-     (if (equal? s s2) dec
+  (define (translate-stmt* s)
+    (match s
+      [(Goto* label) (list (BrLL (% label)))]
+      [(GotoCond* cond iftrue iffalse)
+       (match-let ([(cons new-cond _) (ensure-type (translate-arg cond) bit)])
+         (list (BrCondLL new-cond (% iftrue) (% iffalse))))]
+      [(Return (? void?)) (list (ReturnLL 'void (void)))]
+      [(Return id)
+       (match-let ([(cons ret-id ret-ty) (translate-arg id)])
+         (list (ReturnLL ret-ty ret-id)))]
+      [(Assign target (Read))
+       (match-let ([(cons loc-id loc-ty) (translate-assign-target target)])
+         (list
+          (CallLL i32 (@ 'scanf)
+                  (list (cons "getelementptr inbounds ([5 x i8], [5 x i8]* @.read, i32 0, i32 0)"
+                              (PtrLL byte))
+                        (cons loc-id (PtrLL loc-ty))) #t)))]
+      [(Assign target src)
+       (match-let ([(cons src-id _) (ensure-type (translate-arg src) int)]
+                   [(cons target-id target-ty) (translate-assign-target target)])
+         (list (StoreLL target-ty src-id target-id)))]
+      [(? Inv?)
+       (match-let ([(cons call _) (translate-inv s)])
+         (list call))]
+      [(Delete exp)
+       (match-let ([(cons arg-id arg-ty) (translate-arg exp)])
          (with-tmp (tmp)
-           (add-stmt! (AssignLL tmp (CastLL (if (> s s2) 'trunc 'zext) ty id new-ty)))
-           (cons tmp new-ty)))]
-    [(s (? IntLL?)) dec]
-    [(_ _) (cons id new-ty)]))
+           (list (AssignLL tmp (CastLL 'bitcast arg-ty arg-id (PtrLL byte)))
+                 (CallLL 'void (@ 'free) (list (cons tmp (PtrLL byte))) #f))))]
+      [(Print exp endl?)
+       (let ([arg (translate-arg exp)])
+         (list
+          (CallLL i32 (@ 'printf)
+                  (list (cons
+                         (format "getelementptr inbounds ([6 x i8], [6 x i8]* @.~a, i32 0, i32 0)"
+                                 (if endl? 'println 'print))
+                         (PtrLL byte)) arg) #t)))]))
+
+  ;;
+  (define (translate-assign-target target)
+    (if (Dot? target) (translate-dot target) (hash-ref locs target)))
+
+  ;;
+  (define (translate-arg arg)
+    (match arg
+      [(? boolean?) (cons arg bit)]
+      [(? integer?) (cons arg int)]
+      [(Null) (cons 'null int)]
+      [(? symbol?)
+       (match-let ([(cons id ty) (hash-ref locs arg)])
+         (with-tmp (tmp)
+           (add-stmt! (AssignLL tmp (LoadLL ty id)))
+           (cons tmp ty)))]
+      [(Prim op (list op1 op2))
+       (match-let*
+           ([(list op-name in-ty out-ty) (hash-ref ops op)]
+            [(cons arg1-id op-ty) (ensure-type (translate-arg op1) in-ty)]
+            [(cons arg2-id _)     (ensure-type (translate-arg op2) in-ty)])
+         (with-tmp (tmp)
+           (add-stmt! (AssignLL tmp (BinaryLL op-name op-ty arg1-id arg2-id)))
+           (cons tmp out-ty)))]
+      [(Prim '- (list exp))
+       (match-let ([(cons arg-id _) (translate-arg exp)])
+         (with-tmp (tmp)
+           (add-stmt! (AssignLL tmp (BinaryLL 'sub int 0 arg-id)))
+           (cons tmp int)))]
+      [(Prim '! (list exp))
+       (match-let ([(cons arg-id _) (ensure-type (translate-arg exp) bit)])
+         (with-tmp (tmp)
+           (add-stmt! (AssignLL tmp (BinaryLL 'xor bit #t arg-id)))
+           (cons tmp bit)))]
+      [(Dot left id)
+       (match-let ([(cons dot-id dot-ty) (translate-dot arg)])
+         (with-tmp (tmp)
+           (add-stmt! (AssignLL tmp (LoadLL dot-ty dot-id)))
+           (cons tmp dot-ty)))]
+      [(Inv id args)
+       (match-let ([(cons call ret-ty) (translate-inv arg)])
+         (with-tmp (tmp)
+           (add-stmt! (AssignLL tmp call))
+           (cons tmp ret-ty)))]
+      [(New id)
+       (let* ([ty (translate-type id)]
+              [size (* (/ int-size byte-size) (length (hash-ref structs ty)))])
+         (with-tmp (tmp tmp2)
+           (add-stmt! (AssignLL tmp (CallLL (PtrLL byte) (@ 'malloc) (list (cons size i32)) #f)))
+           (add-stmt! (AssignLL tmp2 (CastLL 'bitcast (PtrLL byte) tmp ty)))
+           (cons tmp2 ty)))]))
+
+  ;;
+  (define+ (translate-dot (Dot left id))
+    (match-let* ([(cons left-id left-ty) (translate-arg left)]
+                 [fields (hash-ref structs left-ty)])
+      (with-tmp (tmp)
+        (add-stmt! (AssignLL tmp (GetEltLL (PtrLL-to left-ty)
+                                           left-id (index-of (map car fields) id))))
+        (cons tmp (cdr (findf (λ+ ((cons mem-id _)) (equal? mem-id id)) fields))))))
+
+  ;;
+  (define+ (translate-inv (Inv id args))
+    (match-let* ([(cons ret-ty param-tys) (hash-ref funs id)])
+      (cons (CallLL ret-ty (@ id)
+                    (map (λ (arg ty) (ensure-type (translate-arg arg) ty)) args param-tys) #f)
+            ret-ty)))
+
+  ;;
+  (define+ (ensure-type (and dec (cons id ty)) new-ty)
+    (match* (ty new-ty)
+      [(type type) dec]
+      [((IntLL size) (IntLL new-size))
+       (with-tmp (tmp)
+         (add-stmt! (AssignLL tmp (CastLL (if (> size new-size) 'trunc 'zext) ty id new-ty)))
+         (cons tmp new-ty))]
+      [(s (? IntLL?)) dec]
+      [(_ _) (cons id new-ty)]))
+  
+  translate-stmt*)
 
 ;;
 (define (translate-fun-vars params decs)
