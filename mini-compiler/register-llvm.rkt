146323166fbedde86e7cec4f0937190b6e48b57f
Alex Maclean (WSL) <alex.maclean2000@gmail.com>
2022-01-29 15:16:03 -0800

    cleanup

--- mini-compiler/register-llvm.rkt
+++ mini-compiler/register-llvm.rkt
@@ -6,7 +6,8 @@
 
 (define return-var '_retval_)
 (define label-prefix 'LU)
-(struct Block (id [preds #:mutable] [phis #:mutable] [stmts #:mutable] [sealed? #:mutable]) #:transparent)
+(struct Block (id [preds #:mutable] [phis #:mutable]
+                  [stmts #:mutable] [sealed? #:mutable]) #:transparent)
 
 ;;
 (define+ (register-llvm (Mini types decs funs))
@@ -17,10 +18,12 @@
         (map translate-global (translate-decs @ decs))
         (map (λ (f) (translate-fun f structs fun-info)) funs)))
 
+;;
 (define+ (translate-fun (Fun id params ret-type decs body) structs fun-info)
   (FunLL (@ id) (translate-decs % params) (translate-type ret-type)
          (translate-cfg params body structs fun-info)))
 
+;;
 (define (translate-cfg params body structs fun-info)
   (define cfg (box '()))
   (define cfg-hash (make-hash))
@@ -39,47 +42,53 @@
     0)
   (unpack-cfg (unbox cfg)))
 
+;;
 (define (unpack-cfg cfg)
   (reverse (map (λ+ ((Block id _ phis stmts _))
                     (BlockLL id (append phis (reverse stmts)))) cfg)))
-  
-  
 
 
-  #;(define+ (control-flow-fun (Fun id params ret-type decs body))
-  (let-values ([(add-block! get-cfg) (make-cfg)])
-    (with-labels (end-id start-id)
-      (add-block! end-id (list (Return (if (equal? ret-type 'void) (void) return-var))))
-      (add-block! start-id ((stmt-cfg add-block! end-id) body (Goto* end-id))))
-    (Fun id params ret-type
-         (if (equal? ret-type 'void) decs (cons (cons return-var ret-type) decs)) (get-cfg))))
 
+
+#;(define+ (control-flow-fun (Fun id params ret-type decs body))
+    (let-values ([(add-block! get-cfg) (make-cfg)])
+      (with-labels (end-id start-id)
+        (add-block! end-id (list (Return (if (equal? ret-type 'void) (void) return-var))))
+        (add-block! start-id ((stmt-cfg add-block! end-id) body (Goto* end-id))))
+      (Fun id params ret-type
+           (if (equal? ret-type 'void) decs (cons (cons return-var ret-type) decs)) (get-cfg))))
+
+;;
 (define (translate-body body current-block add-block! add-stmt! next ret-id)
   (match body
     [(cons (? list? stmts) rest)
      (translate-body (append stmts rest) current-block add-block! add-stmt! next ret-id)]
     [(cons (If guard then else) after)
      (with-labels (then-id else-id after-id)
-         (translate-body then (add-block! then-id #t) add-block! add-stmt! (Goto* after-id) ret-id)
-         (translate-body else (add-block! else-id #t) add-block! add-stmt! (Goto* after-id) ret-id)
-         (translate-body after (add-block! after-id #t) add-block! add-stmt! next ret-id)
-         (add-stmt! current-block (GotoCond* guard then-id else-id)))]
+       (translate-body then (add-block! then-id #t) add-block! add-stmt! (Goto* after-id) ret-id)
+       (translate-body else (add-block! else-id #t) add-block! add-stmt! (Goto* after-id) ret-id)
+       (translate-body after (add-block! after-id #t) add-block! add-stmt! next ret-id)
+       (add-stmt! current-block (GotoCond* guard then-id else-id)))]
     [(cons (While guard while-body) after)
      (with-labels (while-id after-id)
-         (translate-body while-body (add-block! while-id #f) add-block! add-stmt! (GotoCond* guard while-id after-id) ret-id)
-         (translate-body after (add-block! after-id #t) add-block! add-stmt! next ret-id)
-         (add-stmt! current-block (GotoCond* guard while-id after-id)))]
-    [(cons (Return (? void?)) _) (add-stmt! current-block (Goto* ret-id))]
-    [(cons (Return exp) _) (add-stmt! current-block (Assign return-var exp))
-                           (add-stmt! current-block (Goto* ret-id))]
-    [(cons stmt rest) (translate-stmt stmt current-block add-stmt!)
-                      (translate-body rest current-block add-block! add-stmt! next ret-id)]
-    ['() (add-stmt! current-block next)]))
+       (translate-body while-body (add-block! while-id #f) add-block! add-stmt!
+                       (GotoCond* guard while-id after-id) ret-id)
+       (translate-body after (add-block! after-id #t) add-block! add-stmt! next ret-id)
+       (add-stmt! current-block (GotoCond* guard while-id after-id)))]
+    [(cons (Return (? void?)) _)
+     (add-stmt! current-block (Goto* ret-id))]
+    [(cons (Return exp) _)
+     (add-stmt! current-block (Assign return-var exp))
+     (add-stmt! current-block (Goto* ret-id))]
+    [(cons stmt rest)
+     (translate-stmt stmt current-block add-stmt!)
+     (translate-body rest current-block add-block! add-stmt! next ret-id)]
+    ['()
+     (add-stmt! current-block next)]))
 
+;;
 (define (translate-stmt stmt current-block add-stmt!)
   (add-stmt! current-block stmt))
-
-
 
 ;; Macro that given a set of IDs that labels are needed for binds the labels to freshly
 ;; generated labels
